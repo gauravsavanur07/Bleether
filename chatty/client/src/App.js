ea4cfb68b87efaf23e01f68b76b7ab56a99b530e
Gaurav Savanur <gauravsavanur07@gmail.com>
2018-06-25 21:19:02 +0530

    Layout React Navigation

--- chatty/client/src/App.js
+++ chatty/client/src/App.js
@@ -1,63 +1,42 @@
-import{  Platform,
-  StyleSheet,
-  Text,
-  View,
-} from 'react-native';
+import React, { Component } from 'react';
 import { ApolloClient } from 'apollo-client';
 import { ApolloLink } from 'apollo-link';
 import { ApolloProvider } from 'react-apollo';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import { createHttpLink } from 'apollo-link-http';
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import { ReduxCache, apolloReducer } from 'apollo-cache-redux';
 import ReduxLink from 'apollo-link-redux';
 import { onError } from 'apollo-link-error';
+import AppWithNavigationState, {
+  navigationReducer,
+  navigationMiddleware,
+} from './navigation';
 const URL = 'localhost:8080'; // set your comp's url here
 const store = createStore(
   combineReducers({
     apollo: apolloReducer,
+    nav: navigationReducer,
   }),
   {}, // initial state
-  composeWithDevTools(),
+  composeWithDevTools(
+    applyMiddleware(navigationMiddleware),
+  ),
 );
 const cache = new ReduxCache({ store });
-const reduxLink = new ReduxLink(store);
-const errorLink = onError((errors) => {
-  console.log(errors);
-});
-const httpLink = createHttpLink({ uri: `http://${URL}/graphql` });
-const link = ApolloLink.from([
-  reduxLink,
-  errorLink,
-  httpLink,
-]);
-export const client = new ApolloClient({
-  link,
+...
   cache,
-});
-const instructions = Platform.select({
-    'Shake or press menu button for dev menu',
 });
 export default class App extends Component {
   render() {
     return (
-<ApolloProvider client={client}>
+      <ApolloProvider client={client}>
         <Provider store={store}>
-          <View style={styles.container}>
-            <Text style={styles.welcome}>
-              Welcome to React Native!
-            </Text>
-            <Text style={styles.instructions}>
-              To get started, edit App.js
-            </Text>
-            <Text style={styles.instructions}>
-              {instructions}
-            </Text>
-          </View>
+          <AppWithNavigationState />
         </Provider>
       </ApolloProvider>
-);
-
-	}
+    );
+  }
 }
+
